6adff8cc92631ac9342099a2848b64c01b04d94c
Niels Houben <31084063+Nisse2@users.noreply.github.com>
2021-06-01 16:33:46 +0200

    explanatory comments

--- First Game/Assets/Scripts/CheckPoint.cs
+++ First Game/Assets/Scripts/CheckPoint.cs
@@ -7,11 +7,13 @@ public class CheckPoint : MonoBehaviour
     private GameMaster gm;
 
     void Start() {
-        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>(); 
     }
-
-
     // Start is called before the first frame update
+    
+    
+     // when this checkpoint object is colliding with something else (tag==player):
+     //it will store its position as next respawn position within the gamemaster object so that value will survive scene restarts
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) { // jumpboost
             Debug.Log("touched");
